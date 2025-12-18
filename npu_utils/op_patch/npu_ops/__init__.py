0aca243b9ddf60ba04d53e05f0bdb0f8de6191a3
zhangtao0408 <zhangtao529@huawei.com>
2025-12-18 03:38:20 +0000

    [Issue Fix] Delete unused import

--- npu_utils/op_patch/npu_ops/__init__.py
+++ npu_utils/op_patch/npu_ops/__init__.py
@@ -1,6 +1,5 @@
-from npu_utils.op_patch.npu_ops.argsort import patch_argsort
-from npu_utils.op_patch.npu_ops.group_norm import patch_group_norm
 from npu_utils.op_patch.npu_ops.rmsnorm import patch_rmsnorm
+from npu_utils.op_patch.npu_ops.argsort import patch_argsort
 
 __all__ = [
     "patch_rmsnorm",
